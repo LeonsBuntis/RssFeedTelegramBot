46490cef50a0a8b5c16fe20a6b5eca4706943988
Ponyhook <leon.buntis@gmail.com>
2020-06-30 22:44:42 +0300

    test file

--- test.js
+++ test.js
@@ -1,3 +1,16 @@
 const { tryAddChatId, tryRemoveChatId, getChatIds } = require('./pg_repo.js');
 
-tryRemoveChatId('123').then(_ => getChatIds().then(e => console.log(e)))
+// tryRemoveChatId('123').then(_ => getChatIds().then(e => console.log(e)));
+
+
+// const Scraper = require('./scrapper_ss.js');
+
+// let sc = new Scraper('https://www.ss.com/msg/lv/real-estate/flats/riga/centre/eikjg.html');
+
+// sc.scrape((img) => {
+//     let url = new URL(img);
+
+//     if (url.origin === 'https://i.ss.com') {
+//         console.log(url.href);
+//     }
+// });
