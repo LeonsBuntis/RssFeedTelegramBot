f7b127130fa8195f2283e5a59d44d2a7eeb6894c
Ponyhook <leon.buntis@gmail.com>
2020-07-05 22:50:10 +0300

    fix

--- feeders/flatFeeder.js
+++ feeders/flatFeeder.js
@@ -8,12 +8,44 @@ const feeds = [
 ];
 
 class Feeder {
-
     async feed(callback) {
         for (const url of feeds) {
             await handleFeed(url, callback);
         }
     };
+}
+
+class Handler {
+    constructor(item, callback) {
+        this.item = item;
+        this.callback = callback;
+    }
+
+    handleFeed() {
+        let scrapper = new Scraper(this.item.link);
+        this.imgUrls = [];
+
+        scrapper.scrape((address) => {
+            let url = new URL(address);
+
+            if (url.origin === 'https://i.ss.com') {
+                this.imgUrls.push(url.href);
+            }
+        });
+
+        this.content = parseSScontentSnippet(this.item.contentSnippet);
+        this.content.link = this.item.link;
+        this.content.title = this.item.title;
+
+        scrapper.on("end", () => {
+            let distinctImgUrls = [...new Set(this.imgUrls)];
+
+            this.callback({
+                content: this.content,
+                imageUrls: distinctImgUrls
+            });
+        });
+    }
 }
 
 const handleFeed = async (feedUrl, callback) => {
@@ -24,35 +56,14 @@ const handleFeed = async (feedUrl, callback) => {
     for (let i = 0; i < newItems.length; i++) {
         const item = newItems[i];
 
-        let scrapper = new Scraper(item.link);
-        let imgUrls = [];
-
-        scrapper.scrape((address) => {
-            let url = new URL(address);
-
-            if (url.origin === 'https://i.ss.com') {
-                imgUrls.push(url.href);
-            }
-        });
-
-        var content = parseSScontentSnippet(item.contentSnippet);
-        content.link = item.link;
-        content.title = item.title;
-
-        scrapper.on("end", () => {
-            let distinctImgUrls = [...new Set(imgUrls)];
-
-            callback({
-                content: content,
-                imageUrls: distinctImgUrls
-            });
-        });
+        let handler = new Handler(item, callback);
+        handler.handleFeed();
     }
 }
 
 const getNewItems = (items) => {
     const from = new Date();
-    from.setHours(from.getHours() - 1);
+    from.setHours(from.getHours() - 2);
     from.setMinutes(0);
     from.setSeconds(0);
 
